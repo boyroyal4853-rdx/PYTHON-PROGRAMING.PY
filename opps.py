7c41705bbe376502cc2491eee8253c8cc542959c
Diwakar_Chaudhary_07 <boyroyal4853@gmail.com>
2025-12-24 19:32:14 +0530

    add the nfile

--- opps.py
+++ opps.py
@@ -40,28 +40,58 @@
 
 
 
-class calculater():
-       def calculation(calc):
-          x = int(input("enter your first number:"))
-          y = int(input("enter your second number:"))
+# class calculater():
+#        def calculation(calc):
+#           x = int(input("enter your first number:"))
+#           y = int(input("enter your second number:"))
           
-          sum = x+y
-          sub = x-y
-          mul = x*y
-          div = x/y
-          mod = x%y
-          folar_div = x//y
-          expo = x**y
+#           sum = x+y
+#           sub = x-y
+#           mul = x*y
+#           div = x/y
+#           mod = x%y
+#           folar_div = x//y
+#           expo = x**y
 
-          print("the sum  of two number is :", sum)
-          print("the substraction of two number is :", sub)
-          print("the multiplication of two number is :", mul)
-          print("the division of two number is :", div)
-          print("the modulus of two number is :", mod)
-          print("the folar division of two number is :", folar_div)
-          print("the exponentiation of two number is :", expo)
+#           print("the sum  of two number is :", sum)
+#           print("the substraction of two number is :", sub)
+#           print("the multiplication of two number is :", mul)
+#           print("the division of two number is :", div)
+#           print("the modulus of two number is :", mod)
+#           print("the folar division of two number is :", folar_div)
+#           print("the exponentiation of two number is :", expo)
 
 
 
-CALC_OBJ = calculater()
-CALC_OBJ.calculation()   
+# CALC_OBJ = calculater()
+# CALC_OBJ.calculation()   
+
+
+
+class Account:
+            x = "200"
+            y = "500"
+            # x =+y
+
+
+      
+            def bank_details(user):
+                        account_holder_name = " Diwakar Chaudhary"
+                        bank_name = " SBI BANK"
+                        account_number = "xxxxxxxxxxxxxx3252"
+                        ifsc_code = "SBIN0003252"
+                        branch_name = " muzaffarpur mothijheel "
+                        balance = "xxxxx.xx"
+                        print("ACCOUNT HOLDER =>", account_holder_name)
+                        print("BANK NAME=>",bank_name)
+                        print("ACCOUNT NUMBER =>", account_number)   
+                        print("IFSC CODE =>", ifsc_code)
+                        print("BRANCH NAME =>", branch_name)
+                        print("BALANCE => ", balance)
+
+
+bank_obj = Account()
+bank_obj.bank_details()
+
+print(bank_obj.x )
+print(bank_obj.y )
