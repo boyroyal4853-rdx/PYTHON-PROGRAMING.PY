216f0450906e35e999ab75fcd376ec21b1f19059
diwakar chaudhary <boyroyal4853@gmail.com>
2026-07-13 22:31:43 +0530

    add

---
+++ practice/hii .py
@@ -0,0 +1,18 @@
+print("ram worold")
+
+
+x = int(input("enter your number"))
+  
+if x % 2 == 0:
+    print("even number")
+else:
+    print("odd number")
+
+
+
+def add_your_numbers(a,b):
+        print("your sum is:" , a+b)
+
+
+
+add_your_numbers(11,6)
